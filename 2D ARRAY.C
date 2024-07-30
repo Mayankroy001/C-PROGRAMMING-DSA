1e401f9894d11dbfaaa319e5b5eb927c8b66cb43
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-07-30 16:18:47 +0000

    2D ARRAY.C

---
+++ 2D ARRAY.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int marks[4][3]= { {22, 44, 66},
+        {33, 12, 34},
+        {55, 0, 0},
+        {0, 0, 0}};
+    printf("The values : ", marks[4][3]);
+   
+};
+printf("The values are : ");
+for(int i = 0; i < 4; i++){
+ for (int j = 0; j < 4; j++){
+    
+ printf("%d\n",marks[i],[j]);
+}
+
+printf("\n");
+}
+return 0;
+}
