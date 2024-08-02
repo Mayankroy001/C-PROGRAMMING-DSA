02c2bcf5aa29b888081aaf4db69856efbdfc3ad1
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-08-02 14:30:27 +0000

    2D ARRAY.C

--- .vscode/ARRAY.C/2D ARRAY.C
+++ .vscode/ARRAY.C/2D ARRAY.C
@@ -2,17 +2,15 @@
 int main()
 {
     int marks[4][3]= { {22, 44, 66},
-        {33, 12, 34},
-        {55, 0, 0},
-        {0, 0, 0}};
-    printf("The values : ", marks[4][3]);
+                       {33, 12, 34},
+                       {55, 0, 0},
+                       {0, 0, 0}};
+    printf("The values : \n");
    
-};
-printf("The values are : ");
 for(int i = 0; i < 4; i++){
  for (int j = 0; j < 4; j++){
     
- printf("%d\n",marks[i],[j]);
+ printf("%d\n",marks[i][j]);
 }
 
 printf("\n");
