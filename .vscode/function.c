1f07604f64051f52ad0b187c328b8882e835540e
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-08-12 19:57:51 +0000

    function.c

---
+++ .vscode/function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int sum(int a,int b)
+{
+    return a+b;
+}
+
+int main(){
+int a,b;
+int c= sum(a,b);
+printf("Enter the value of a: ");
+scanf("%d",&a);
+
+printf("Enter the value of b: ");
+scanf("%d",&b);
+
+printf("The sum value is: %d\n ",c);
+
+
+return 0;
+}
