f5af7cb6d1c6e0ac2a18c510b646aa420839c32a
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-08-12 20:15:24 +0000

    function multiply.c

---
+++ .vscode/function multiply.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+multi(int a, int b)
+{
+    return a*b;
+}
+
+
+int main(){
+    int a,b;
+   
+   
+    printf("Enter the value of a : ");
+    scanf("%d",&a);
+    printf("Enter the value of b : ");
+    scanf("%d",&b);
+
+    int c= multi(a,b);
+  printf("The value after muktiply : %d\n",c);
+  return 0;
+}
