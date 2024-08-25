e764bbc04dd261608c812dc70efc72362f916020
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-08-25 11:56:25 +0000

    1st pointer.c

--- .vscode/array practice/functions/structure/pointer/1st pointer.c
+++ .vscode/array practice/functions/structure/pointer/1st pointer.c
@@ -6,6 +6,7 @@ x=10;
 int*ptr; //declare ponter
 ptr=&x;
 printf("%d\n",x); //output for intiger value
-printf("%p",&x);  //output for pointer value
+printf("%p\n",&x);  //output for pointer value
+printf("%p",ptr);  //another way to ouput pointer
 return 0;
 }
