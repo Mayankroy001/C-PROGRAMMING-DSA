021dbd09cc350146ab0ae7b3ff48649776b09dc3
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-08-25 11:54:57 +0000

    1st pointer.c

--- .vscode/array practice/functions/structure/pointer/1st pointer.c
+++ .vscode/array practice/functions/structure/pointer/1st pointer.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
+int main(){
+int x;
+x=10;
 
-int main() {  // Corrected function name from mmain to main
-    int x;
-    x = 10;
-    int *ptr;
-    ptr = &x;
-
-    printf("%d\n", x);     // Print the value of x
-    printf("%p\n", &x);    // Print the address of x (use %p for pointers)
-
-    return 0;
+int*ptr; //declare ponter
+ptr=&x;
+printf("%d\n",x); //output for intiger value
+printf("%p",&x);  //output for pointer value
+return 0;
 }
