1775ec112613e02b243241a94ece81e7cd0e108d
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-08-26 12:30:08 +0000

    pointer practice.c

---
+++ .vscode/pointer practice.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int a[5]={1,2,3,4,5};
+    int i, sum =0;
+    for(i=0;i<5;i++){
+
+    sum =sum + *(a+i);
+
+}
+
+     printf("Sum : %d\n", sum);
+     return 0;
+}
